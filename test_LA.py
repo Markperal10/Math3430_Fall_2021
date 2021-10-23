1597328cbae7a0efec398437972002a4cc81ad91
Markperal10 <90790921+Markperal10@users.noreply.github.com>
2021-10-22 19:41:08 -0500

    Homework 04 Submission Mark Peralta

--- test_LA.py
+++ test_LA.py
@@ -50,5 +50,45 @@ def test_matr_matr_multi():
     matrix2 = [[2, 4], [5, 7]]
     matrix3 = [[8, 2], [3, 1]]
     matrix4 = [[1, 6], [9, 7]]
-    assert LA.matr_matr_multi(matrix1,matrix2) == [[26,32],[50,62]]
-    assert LA.matr_matr_multi(matrix3,matrix4) == [[26,8],[93,25]] 
+    assert LA.matr_matr_multi(matrix1, matrix2) == [[26, 32], [50, 62]]
+    assert LA.matr_matr_multi(matrix3, matrix4) == [[26, 8], [93, 25]]
+
+
+def test_abs_value():
+    scalar1 = -5
+    complex = 3 - 4j
+    assert LA.abs_value(scalar1) == 5
+    assert LA.abs_value(complex) == 5
+
+
+def test_p_norm():
+    p: int = 2
+    vector_A = [1, 2, 3]
+    vector_B = [2, 4, 6]
+    assert LA.p_norm(vector_A, p) == 3.7416573867739413
+    assert LA.p_norm(vector_B, p) == 7.483314773547883
+
+
+def test_inf_norm():
+    vector_1 = [4, 7, -9, 1]
+    vector_2 = [3, -5, 7, 2]
+    assert LA.inf_norm(vector_1) == 9
+    assert LA.inf_norm(vector_2) == 7
+
+
+def test_p_inf_norm_result():
+    vector_B = [2, 4, 6]
+    p: int = 2
+    boolean = False
+    boolean2 = True
+    assert LA.P_inf_norm_result(vector_B, p, boolean) == 7.483314773547883
+    assert LA.P_inf_norm_result(vector_B, p, boolean2) == 6
+
+
+def test_inner_product():
+    vector1 = [3,4,5]
+    vector2 = [2,1,4]
+    vector3 = [7,8,9]
+    assert LA.Inner_product(vector1,vector2) == 30
+    assert LA.Inner_product(vector2,vector3) == 58
+    
