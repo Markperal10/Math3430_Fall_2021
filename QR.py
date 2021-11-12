1fa1f0bc760e768c6848fe3c02880cfcac7ddf75
Markperal10 <90790921+Markperal10@users.noreply.github.com>
2021-11-12 12:39:16 -0600

    Homework 06 Submission
    I am submitting this homework late as I attended the services for my family last week.

--- QR.py
+++ QR.py
@@ -1,43 +1,6 @@
 import LA
 
-
-# Problem 1
-def unstable_Gram_Schmidt(matrix1: list[list[complex]])->list[list[complex]]:
-    """ Finds the Q and R for QR factorization using the unstable Gram Schmidt method
-    Creates four seperate variables, Q being a blank list, V being a matrix stored as the same size as matrix,
-    R being stored as a matrix using the same amount of columns from matrix1, and a scalar which is a blank list.
-    The first for loop will then run for the number of columns in matrix1.
-    Within the for loop V[outer_for] is replaced with matrix1[outer_for]. Within that for loop another for loop is created
-    and will run within the range of zero to what the outer for loop is at.
-    This for loop then stores the product of Q and V using out function from our LA.py file.
-    From there it will compute the the scalar vector multiplication of Q and -R form out scalar_vec_multi function from LA.py
-    From there V is then set to equal the sum of the vector V and the scalar using the add_vectors function from LA.py.
-    R is then set to equal the p norm of V using the p_norm function from LA.py.Then the scalar_vec_multi  of V and 1/R
-    is then appended to Q.
-
-     Args:
-         matrix1: A matrix stored as a list of lists with complex and real numbers
-
-     Returns:
-         The Q and R for QR factorization
-
-    """
-    Q: list[float] = []
-    V: list[list[float]] = [([0] * (len(matrix1[0]))) for i in range(len(matrix1))]
-    R: list[list[float]] = [([0] * (len(matrix1))) for i in range(len(matrix1))]
-    scalar: list[float] = []
-
-    for outer_for in range(len(matrix1)):
-        V[outer_for] = matrix1[outer_for]
-        for inner_for in range(0,outer_for):
-            R[outer_for][inner_for] = LA.Inner_product(Q[inner_for],V[outer_for])
-            scalar = LA.scalar_vec_multi(Q[inner_for], -R[outer_for][inner_for])
-            V[outer_for] = LA.add_vectors(V[outer_for],scalar)
-        R[outer_for][outer_for] = LA.p_norm((V[outer_for]))
-        Q.append(LA.scalar_vec_multi((V[outer_for]),(1/R[outer_for][outer_for])))
-    return [Q,R]
-
-#Problem 2
+#Problem 1
 def stable_Gram_Schmidt(matrix1:list[list[float]])->list[list[float]]:
     """ Finds the Q and R for QR factorization using the stable Gram Schmidt method
     Creates four seperate variables, Q being a blank list, V being a matrix stored as the same size as matrix,
@@ -71,5 +34,25 @@ def stable_Gram_Schmidt(matrix1:list[list[float]])->list[list[float]]:
             vh = LA.scalar_vec_multi(Q[outer_for], -R[outer_for][inner_for])
             V[inner_for] = LA.add_vectors(V[inner_for],vh)
     return [Q,R]
+
+# Problem 2
+
+def orthonormal_vectors(matrix1: list[list[complex]])-> list[list[complex]]:
+    """Returns an orthonormal list of vectors from our input matrix
+    Q is set to equal the stable_Gram_schmidt function which is from our QR file. Using the
+    stable_Gram_Schmidt we can already calculate the orthonormal list of vectors for our input matrix
+    which is Q. We only want to return Q, so only the first column in Q noted as Q[0] which stores the list
+    that e want to return.
+
+    Args:
+        matrix1: A amtrix stored as a list of lists with complex and real numbers
+
+    Returns:
+        Q which is set to equal the orthonormal list of vectors of the matrix inputted.
+
+    """
+    Q : list[list[complex]] = stable_Gram_Schmidt(matrix1)
+    return Q[0]
+
 
 
